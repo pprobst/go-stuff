0ee6da80471c5efb0da2ba74757fab24a38e3996
pprobst <pprobst@insiberia.net>
2019-07-26 17:00:54 -0300

    directional channels

---
+++ concurrency/directional_channels.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	r := make(<-chan int) // receiver channel
+	s := make(chan<- int) // sender channel
+
+	fmt.Printf("c: %T\n", r)
+	fmt.Printf("s: %T\n", s)
+
+	c := make(chan int)
+
+	// send
+	go foo(c)
+
+	// receive
+	bar(c)
+}
+
+// send
+func foo(c chan<- int) {
+	c <- 42
+}
+
+// receive
+func bar(c <-chan int) {
+	fmt.Println(<-c)
+}
