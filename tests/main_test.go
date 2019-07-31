25a155ee64eda19f06a109dd7e926d66004d9b40
pprobst <pprobst@insiberia.net>
2019-07-31 15:18:23 -0300

    table testing

--- tests/main_test.go
+++ tests/main_test.go
@@ -5,8 +5,22 @@ import (
 )
 
 func TestMySum(t *testing.T) {
-    s := mySum(2, 3)
-    if s != 5 {
-        t.Error("Expected", 5, "Got", s) 
+
+    type test struct {
+        data []int
+        answer int
+    }
+
+    tests := []test{
+        test{[]int{21, 21}, 42},
+        test{[]int{16, 0}, 16},
+        test{[]int{-5, -50}, -55},
+    }
+
+    for _, v := range tests {
+        x := mySum(v.data...)
+        if x != v.answer {
+            t.Error("Expected", v.answer, "Got", x) 
+        }
     }
 }
